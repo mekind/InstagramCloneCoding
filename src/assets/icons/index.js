ed03bf66a6b2741f6717fd1747226b1912411c57
kds <mekind98@gmail.com>
2022-10-16 13:39:46 +0900

    1

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,7 +2,43 @@ import AddIcon from './add.png';
 import LikeIcons from './likes.png';
 import DefaultProfile from './user.png';
 import PlaneIcon from './plane.png';
+import Test1 from './test1.jpeg';
+import Test2 from './test2.jpeg';
+import Test3 from './test3.jpeg';
+import Bookmark from './bookmark.png';
+import Comment from './comment.png';
+import ShopIcon from './shop.png';
+import HomeIcon from './home.png';
+import SearchIcon from './search.png';
 import ReelsIcon from './reels.png';
-import Test from './test1.jpeg';
+import Jo from './jo.jpeg';
 
-export {AddIcon, LikeIcons, PlaneIcon, ReelsIcon, DefaultProfile, Test};
+import Testuser1 from './testuser1.jpeg';
+import Testuser2 from './testuser2.jpeg';
+import Testuser3 from './testuser3.jpeg';
+import Testuser4 from './testuser4.jpeg';
+import Testuser5 from './testuser5.jpg';
+
+// import ProfileIcon from './proflie.png';
+
+export {
+  AddIcon,
+  LikeIcons,
+  PlaneIcon,
+  DefaultProfile,
+  Test1,
+  Test2,
+  Test3,
+  Bookmark,
+  Comment,
+  HomeIcon,
+  SearchIcon,
+  ReelsIcon,
+  ShopIcon,
+  Jo,
+  Testuser1,
+  Testuser2,
+  Testuser3,
+  Testuser4,
+  Testuser5,
+};
